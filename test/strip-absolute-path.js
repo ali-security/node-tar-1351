d612e386a4ebe6115be4f79e5c21944c129e2a0e
Tal Samucha <tal.s@sealsecurity.io>
2025-11-17 22:13:48 +0200

    more addition

--- test/strip-absolute-path.js
+++ test/strip-absolute-path.js
@@ -2,20 +2,21 @@ var t = require('tap')
 var stripAbsolutePath = require('../lib/strip-absolute-path.js')
 
 var cases = {
-  '/': ['/', ''],
-  '////': ['////', ''],
-  'c:///a/b/c': ['c:///', 'a/b/c'],
-  '\\\\foo\\bar\\baz': ['\\\\foo\\bar\\', 'baz'],
-  '//foo//bar//baz': ['//', 'foo//bar//baz'],
-  'c:\\c:\\c:\\c:\\\\d:\\e/f/g': ['c:\\c:\\c:\\c:\\\\d:\\', 'e/f/g'],
+    '/': ['/', ''],
+    '////': ['////', ''],
+    'c:///a/b/c': ['c:///', 'a/b/c'],
+    '\\\\foo\\bar\\baz': ['\\\\foo\\bar\\', 'baz'],
+    '//foo//bar//baz': ['//', 'foo//bar//baz'],
+    'c:\\c:\\c:\\c:\\\\d:\\e/f/g': ['c:\\c:\\c:\\c:\\\\d:\\', 'e/f/g'],
 }
 
 var keys = Object.keys(cases)
 for (var i = 0; i < keys.length; i++) {
-  var input = keys[i]
-  var expected = cases[input]
-  var root = expected[0]
-  var stripped = expected[1]
-  t.strictSame(stripAbsolutePath(input), [root, stripped], input)
+    var input = keys[i]
+    var expected = cases[input]
+    var root = expected[0]
+    var stripped = expected[1]
+    var result = stripAbsolutePath(input)
+    t.equivalent(result, [root, stripped], input)
 }
 
