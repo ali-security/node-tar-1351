ce7d43c34883cd0981d6bf27cdd320e988446e68
Tal Samucha <tal.s@sealsecurity.io>
2025-11-17 15:55:45 +0200

    CVE-2021-32804_test

---
+++ test/strip-absolute-path.js
@@ -0,0 +1,18 @@
+var t = require('tap')
+var stripAbsolutePath = require('../lib/strip-absolute-path.js')
+
+var cases = {
+  '/': ['/', ''],
+  '////': ['////', ''],
+  'c:///a/b/c': ['c:///', 'a/b/c'],
+  '\\\\foo\\bar\\baz': ['\\\\foo\\bar\\', 'baz'],
+  '//foo//bar//baz': ['//', 'foo//bar//baz'],
+  'c:\\c:\\c:\\c:\\\\d:\\e/f/g': ['c:\\c:\\c:\\c:\\\\d:\\', 'e/f/g'],
+}
+
+for (var input in cases) {
+  var expected = cases[input]
+  var result = stripAbsolutePath(input)
+  t.strictSame(result, expected, input)
+}
+
