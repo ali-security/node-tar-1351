b84452bbfbf8a3ba6e96f0c1a0100edcb8b66194
Tal Samucha <tal.s@sealsecurity.io>
2025-11-17 15:58:36 +0200

    update

--- test/strip-absolute-path.js
+++ test/strip-absolute-path.js
@@ -2,17 +2,17 @@ var t = require('tap')
 var stripAbsolutePath = require('../lib/strip-absolute-path.js')
 
 var cases = {
-  '/': ['/', ''],
-  '////': ['////', ''],
-  'c:///a/b/c': ['c:///', 'a/b/c'],
-  '\\\\foo\\bar\\baz': ['\\\\foo\\bar\\', 'baz'],
-  '//foo//bar//baz': ['//', 'foo//bar//baz'],
-  'c:\\c:\\c:\\c:\\\\d:\\e/f/g': ['c:\\c:\\c:\\c:\\\\d:\\', 'e/f/g'],
+    '/': ['/', ''],
+    '////': ['////', ''],
+    'c:///a/b/c': ['c:///', 'a/b/c'],
+    '\\\\foo\\bar\\baz': ['\\\\foo\\bar\\', 'baz'],
+    '//foo//bar//baz': ['//', 'foo//bar//baz'],
+    'c:\\c:\\c:\\c:\\\\d:\\e/f/g': ['c:\\c:\\c:\\c:\\\\d:\\', 'e/f/g'],
 }
 
 for (var input in cases) {
-  var expected = cases[input]
-  var result = stripAbsolutePath(input)
-  t.strictSame(result, expected, input)
+    var expected = cases[input]
+    var result = stripAbsolutePath(input)
+    t.strictSame(result, expected, input)
 }
 
